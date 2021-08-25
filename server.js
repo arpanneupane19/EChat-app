3d08d06e742ecd985431b9607be0347e4610a2df
Arpan Neupane <arpanneupane19@gmail.com>
2021-08-25 00:37:17 -0500

    fixed messaging

--- server.js
+++ server.js
@@ -62,17 +62,61 @@ app.get("/logout", (req, res) => {
 // Socket IO for Messaging
 const userToRoom = new Map();
 const roomToUsers = new Map();
-const userToId = new Map();
+const idToUser = new Map();
 
 io.on('connection', (socket) => {
     console.log("Made a connection.")
 
+    socket.on('connectUser', (data) => {
+        userToRoom.set(data.user, data.room)
+        console.log('User to room connection', userToRoom)
+
+        if (roomToUsers.get(data.room) === undefined) {
+            roomToUsers.set(data.room, [data.user]);
+        }
+        else {
+            let usersInRoom = roomToUsers.get(data.room);
+            usersInRoom.push(data.user)
+            roomToUsers.set(data.room, usersInRoom);
+        }
+        console.log('Room to user connection', roomToUsers)
+
+        idToUser.set(socket.id, data.user)
+        console.log('SocketID to user', idToUser)
+
+        socket.join(data.room)
+    })
+
     socket.on("chat", (data) => {
         Message.create({ message: data.message, sender: data.sender, receiver: data.receiver, roomCode: data.roomCode }).catch(err => { console.log(err) })
-        socket.emit("message", {
+        io.in(data.roomCode).emit("message", {
             message: data.message,
             sender: data.sender
         })
+    })
+
+    socket.on('disconnect', (reason) => {
+        if (idToUser.has(socket.id)) {
+            const username = idToUser.get(socket.id)
+            const room = userToRoom.get(username)
+            const usersInRoom = roomToUsers.get(room)
+
+            userToRoom.delete(username)
+            console.log('Username to Room', userToRoom)
+
+            if (usersInRoom.includes(username)) {
+                let index = usersInRoom.indexOf(username)
+                usersInRoom.splice(index, 1)
+            }
+            if (usersInRoom.length === 0) {
+                roomToUsers.delete(room)
+            }
+
+            idToUser.delete(socket.id)
+            console.log('Room to user', roomToUsers)
+            console.log('Id to User', idToUser)
+        }
+
     })
 
 })
